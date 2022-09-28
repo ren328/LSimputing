47db40739060d447e4f02073fcdb4d7993545da7
ren328 <57877581+ren328@users.noreply.github.com>
2022-09-28 08:44:33 -0500

    Update RF.py

--- RF.py
+++ RF.py
@@ -14,15 +14,15 @@ import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
-##load snp matrix
+##load snp matrix,already fill missing values and proper scaled
 ##change the path to your own, use proper function to load the snp
-##
 snp_test="snp_path"
 
 
 
-##load trait
-trait=np.loadtxt("trait_truth")
+##load trait,proper scaled, use proper function to load the trait
+##change the path to your own
+trait="trait_truth"
 
 
 X_train,X_test,y_train,y_test=train_test_split(snp_test,trait,test_size=0.3,random_state=1)
@@ -30,7 +30,6 @@ X_train,X_test,y_train,y_test=train_test_split(snp_test,trait,test_size=0.3,rand
 forest=RandomForestRegressor(n_estimators=100,criterion='mse',random_state=1,n_jobs=-1)
 forest.fit(X_train,y_train)
 y_test_pred=forest.predict(X_test)
-test_cor=np.corrcoef(y_test_pred,y_test)
 
 ##change path to save the prediction result
 np.savetxt("path",y_test_pred)
