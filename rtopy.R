f3f65fa34fa60deb87d6e1190bc740920396091f
ren328 <57877581+ren328@users.noreply.github.com>
2022-09-28 14:01:35 -0500

    Update rtopy.R

--- rtopy.R
+++ rtopy.R
@@ -1,6 +1,6 @@
 f0<-function(batchsize){
   ##load SNP matrix, should be a .RData file, for diffrent batches, change the RData_path to your own accordingly
   load(file="RData_path")
-  
+  ##change the snp matrix name to your own
   return("snp matrix's name in .RData"/(batchsize-1))
 }
