cbe4073b5bc558108b066255b204ba101a370aa9
ren328 <57877581+ren328@users.noreply.github.com>
2022-11-29 20:04:30 -0600

    Update Gety.py

--- Computational Methods/Gety.py
+++ Computational Methods/Gety.py
@@ -6,12 +6,14 @@ from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 
 ##This file is to get the result for one batch, to get the result for all sample, change the cp_path accordingly
+
+
 ##input batchsize
 batchsize=
 def my_loss(y_true, y_pred):
     squared_difference = tf.square(y_true - y_pred)
     return (tf.reduce_sum(squared_difference))
-
+##change the parameters to your own setting
 model = keras.Sequential([layers.Dense(units=1, activation = "linear",kernel_regularizer=regularizers.l2(l=0))])
 opt = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=opt,loss = my_loss)
