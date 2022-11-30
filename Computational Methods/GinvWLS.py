5ca4459ea223c617d5d029592bd1a815005896d7
ren328 <57877581+ren328@users.noreply.github.com>
2022-11-29 20:25:35 -0600

    Update GinvWLS.py

--- Computational Methods/GinvWLS.py
+++ Computational Methods/GinvWLS.py
@@ -1,15 +1,24 @@
-beta=np.loadtxt("/panfs/jay/groups/20/panwei/ren00075/1003batchsize/betafinalsdwls.txt")
-  
-print(beta.shape)
-print(type(beta))      
-sd=np.loadtxt("/panfs/jay/groups/20/panwei/ren00075/1003batchsize/sdfinalsdwls.txt")
+import math
+import scipy
+import scipy.linalg 
+import tensorflow as tf
+import numpy as np
+import json
+import os
+import pandas as pd
+import time
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 
-n=178176
-batch=25000
-p=50000
-batch_num=math.ceil(n/batch)
+
+##Use proper function to load SNP, should be NA filled and centered
+snp="path"
+snp=np.array(snp)
+p=np.shape(snp)[1]
+
 res=[]
-ctime=time.process_time()
+time1=time.process_time()
 for j in range(p):
   c=snp[:,j]
   cp=np.matmul(c.T,c)
@@ -17,8 +26,25 @@ for j in range(p):
   res.append(cp1)
 
 res1=np.diag(res)
-print(res1.shape)
-##p*p * p*n
+cxt=np.matmul(res1,snp.T)
 cxt1=np.matmul(res1,snp.T)
 
 cxt=np.divide(cxt1.T,sd).T
+
+##Use proper funciton to load GWAS summary statistics
+beta="path"
+sd="path"
+
+xxt=np.matmul(cxt.T,cxt)
+
+xxtinv=np.linalg.pinv(xxt,rcond=math.sqrt(np.finfo(float).eps))
+
+reg=np.matmul(xxtinv,cxt.T)
+
+yhat=np.matmul(reg,beta)
+##end recording time
+time2=time.process_time()
+chtime=[time2-time1]
+##use proper function to save the result
+np.savetxt("path",chtime)
+np.savetxt("path",yhat)
