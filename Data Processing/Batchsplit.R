22268019cc63d18950f91dbed879f3cde511a7a5
ren328 <57877581+ren328@users.noreply.github.com>
2023-04-09 18:37:37 -0500

    Update Batchsplit.R

--- Data Processing/Batchsplit.R
+++ Data Processing/Batchsplit.R
@@ -1,5 +1,7 @@
 ##use proper function to load snp matrix, change path to your own
 snp_test="path"
+##center(T) or standardization(F)
+center=T
 ##fill missing values
 snp<-apply(snp_test, 2, function(x) replace(x, is.na(x), mean(x, na.rm = TRUE)))
 snp<-data.frame(snp)
@@ -24,7 +26,14 @@ datasplit<-function(chunksize,id,data){
 data<-datasplit(chunksize,id,snp1)
 
 ##centered(scale=F)/standardized(scale=T) by batch
+if(center=F){
+snp_final<-lapply(data, function(y) {
+  apply(y, 2, function(z) {
+    (z - mean(z)) / (sd(z) * as.logical(sd(z)))
+  })
+})}else{
 snp_final<-lapply(data,scale,scale=F)
+}
            
 ##save the result
 save(snp_final,file="path")
